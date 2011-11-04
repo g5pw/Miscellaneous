e373e931ebec922b1d3084ec72a59ead9ba17080
Aljaž Srebrnič <a2piratesoft@gmail.com>
2011-11-04 16:29:34 +0100

    ProjectRoulette
    Filtering is preformed directly with grep and now with another for loop
    
    Signed-off-by: Aljaž Srebrnič <a2piratesoft@gmail.com>

--- ProjectRoulette.sh
+++ ProjectRoulette.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 for project in $(find  ~/Documents/Projects/ -type dir -depth 1); do
-	tags=`mdls -raw -name kOMUserTags $project | sed -e '1d' -e '$d' -e 's/^ *//g' -e 's/,//g' -e 's/"//g'`
-	for tag in $tags; do
-		if [[ $tag = 'active' ]]; then
-			active=( ${active[@]-} $(echo $project) )
-			break
-		fi
-	done
+    temp=`mdls -raw -name kOMUserTags $project | grep 'active'`
+    if [[ -n $temp ]]; then
+            active=( ${active[@]-} $(echo $project) )
+    fi
 done
 numProjects=${#active[@]}
 weekNum=`date +%W`
