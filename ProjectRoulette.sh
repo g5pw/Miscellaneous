e0bce96afb4b4300ee1e8028ff816e5a334c922a
Aljaž "g5pw" Srebrnič <a2piratesoft@gmail.com>
2012-06-05 23:24:17 +0200

    Moved script to zsh.

--- ProjectRoulette.sh
+++ ProjectRoulette.sh
@@ -1,12 +1,22 @@
-#!/bin/bash
-for project in $(find  ~/Documents/Projects/ -type dir -depth 1); do
-    temp=`mdls -raw -name kOMUserTags $project | grep 'active'`
-    if [[ -n $temp ]]; then
-            active=( ${active[@]-} $(echo $project) )
+#!/usr/bin/env zsh -f
+# vim: ft=zsh
+for project in $HOME/Documents/Projects/*(/); do
+    if [[ -n $($HOME/.bin/openmeta -t -p $project | grep active) ]]; then
+            active=( $active $(echo $project) )
     fi
 done
+case $1 in
+	week )
+		source=$(date +%W);;
+	day )
+		source=$(date +%d);;
+	* )
+		echo "Usage: $0 week|day"
+		exit;;
+esac
+
 numProjects=${#active[@]}
-weekNum=`date +%W`
-focusing=`expr $weekNum % $numProjects`
+echo "Got $numProjects active projects." 1>&2
+focusing=$(($source % $numProjects))
 focusingName=`basename ${active[$focusing]}`
-echo Focusing on: $focusingName
+echo $focusingName
